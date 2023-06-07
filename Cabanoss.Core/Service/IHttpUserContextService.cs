4fd89a74f2c2cb05e5d45ac3479345c9d5d971fb
JakubRoss <Jakub.Rosploch@gmail.com>
2023-06-07 23:56:19 +0200

    HttpContextUser service

---
+++ Cabanoss.Core/Service/IHttpUserContextService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace Cabanoss.Core.Service
+{
+    public interface IHttpUserContextService
+    {
+        ClaimsPrincipal User { get; }
+        int? UserId { get; }
+        string UserLogin { get; }
+    }
+}
