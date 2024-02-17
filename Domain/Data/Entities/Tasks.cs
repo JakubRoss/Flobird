0c442c85f43bb416901eb8fd83f5b59de622be03
JakubRoss <Jakub.Rosploch@gmail.com>
2024-02-17 23:16:55 +0100

    determining the size of tables

--- Domain/Data/Entities/Tasks.cs
+++ Domain/Data/Entities/Tasks.cs
@@ -15,7 +15,6 @@
         public new int Id { get; set; }
         public string Name { get; set; }
         public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
 
         //Navigation
         public int CardId { get; set; }
