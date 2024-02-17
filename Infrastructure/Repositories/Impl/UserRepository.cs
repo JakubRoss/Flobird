3839d178d867e4f2538192353fa8619838388216
JakubRoss <Jakub.Rosploch@gmail.com>
2024-02-17 17:58:14 +0100

    userrepository slimming

--- Infrastructure/Repositories/Impl/UserRepository.cs
+++ Infrastructure/Repositories/Impl/UserRepository.cs
@@ -7,10 +7,6 @@ namespace Infrastructure.Repositories.Impl
     public class UserRepository : Repository<User>, IUserRepository
     {
         public UserRepository(DatabaseContext context) : base(context){}
-        public DatabaseContext DatabaseContext
-        {
-            get { return Context as DatabaseContext; }
-        }
         /// <summary>
         /// If we use GetUserAsync without the search phrase (null), we get the entire list of users.
         /// otherwise, if we enter the search phrase, we will get all users who contain the "search phrase" in their login or email
@@ -19,8 +15,9 @@ namespace Infrastructure.Repositories.Impl
         /// <returns></returns>
         public async Task<List<User>> GetUsersAsync(string? searchPhrase)
         {
-            var users =  await DatabaseContext.Users.Where(x=>searchPhrase == null || x.Login.ToLower().Contains(searchPhrase) || (x.Email != null && x.Email.Contains(searchPhrase)))
-                .ToListAsync();
+            var users = await this.GetAllAsync(x =>
+                searchPhrase == null || x.Login.ToLower().Contains(searchPhrase) ||
+                (x.Email != null && x.Email.Contains(searchPhrase)));
             return users;
         }
     }
