1f6ea5f03a56c8001a95b72121ae0e29d0bee570
JakubRoss <Jakub.Rosploch@gmail.com>
2024-03-16 18:25:06 +0100

    change token lifetime and claims type

--- Domain/Authentication/AuthenticationService .cs
+++ Domain/Authentication/AuthenticationService .cs
@@ -33,17 +33,17 @@ namespace Domain.Authentication
 
             var claims = new List<Claim>()
             {
-                new(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new(ClaimTypes.Name, user.Login),
-                //new(ClaimTypes.Email, user.Email)
+                new("NameIdentifier", user.Id.ToString()),
+                new("Name", user.Login)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
             var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var expires = DateTime.Now.AddDays(_authenticationSettings.JwtExpireDays);
+            var expires = DateTime.Now.AddMinutes(_authenticationSettings.JwtExpireTime);
 
-            var token = new JwtSecurityToken(_authenticationSettings.JwtIssuer,
+            var token = new JwtSecurityToken(
                 _authenticationSettings.JwtIssuer,
+                _authenticationSettings.JwtAudience,
                 claims,
                 expires: expires,
                 signingCredentials: cred);
